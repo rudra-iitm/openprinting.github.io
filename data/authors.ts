a9087ca0f0d9080ce2bdf3e6bc339db904017b63
Gati Varshney <gativarshney2006@gmail.com>
2026-03-19 12:07:46 +0530

    fix(authors): update bios to match source, enhance links

--- data/authors.ts
+++ data/authors.ts
@@ -1,36 +1,60 @@
 export interface Author {
   key: string;
   name: string;
-  role?: string;
+  bio?: string;
   location?: string;
   email?: string;
   github?: string;
+  links?: {
+    label: string;
+    href: string;
+    kind?: "website" | "linkedin" | "mastodon" | "launchpad" | "other";
+  }[];
   image?: string;
 }
-
 
 const authors: Author[] = [
   {
     key: "Till",
     name: "Till Kamppeter",
-    role: "OpenPrinting Project Leader",
+    bio: "OpenPrinting Organization Lead",
     location: "Vienna, Austria",
     email: "mailto:till.kamppeter@gmail.com",
     github: "https://github.com/tillkamppeter",
+    links: [
+      {
+        label: "LinkedIn",
+        href: "https://linkedin.com/in/kamppetertill",
+        kind: "linkedin",
+      },
+      {
+        label: "Mastodon",
+        href: "https://ubuntu.social/@till",
+        kind: "mastodon",
+      },
+      {
+        label: "Launchpad",
+        href: "https://launchpad.net/~till-kamppeter",
+        kind: "launchpad",
+      },
+    ],
     image: "/authors/till-kamppeter.jpg",
   },
   {
     key: "Mike",
     name: "Michael Sweet",
-    role: "Author of CUPS and PAPPL",
+    bio: "Author of CUPS and PAPPL",
     location: "Canada",
     github: "https://github.com/michaelrsweet",
+    links: [
+      { label: "Website", href: "https://www.msweet.org/", kind: "website" },
+    ],
     image: "/authors/michael-sweet.jpg",
   },
   {
     key: "Aveek",
     name: "Aveek Basu",
-    role: "OpenPrinting Program Manager",
+    bio: "OpenPrinting Program Manager",
     location: "Kolkata, India",
     email: "mailto:basu.aveek@gmail.com",
     github: "https://github.com/AveekBasu",
@@ -39,16 +63,23 @@ const authors: Author[] = [
   {
     key: "Dheeraj",
     name: "Dheeraj",
-    role: "Sub Coordinator, Programming Club, IIT Mandi",
+    bio: "Sub Coordinator, Programming Club, IIT Mandi",
     location: "Mandi, 175001, India",
     email: "mailto:dhirajyadav135@gmail.com",
     github: "https://github.com/dheeraj135",
+    links: [
+      {
+        label: "LinkedIn",
+        href: "https://linkedin.com/in/dheeraj-yadav-282330154",
+        kind: "linkedin",
+      },
+    ],
     image: "/authors/dheeraj135.jpg",
   },
   {
     key: "Zdenek",
     name: "Zdenek Dohnal",
-    role: "Member of CUPS Release Managers group, RHEL/CentOS Stream/Fedora CUPS maintainer",
+    bio: "Member of CUPS Release Managers group, RHEL/CentOS Stream/Fedora CUPS maintainer",
     location: "Brno, Czech Republic",
     github: "https://github.com/zdohnal",
     image: "/authors/placeholder.jpg",
@@ -56,10 +87,22 @@ const authors: Author[] = [
   {
     key: "CNihelton",
     name: "Carlos Nihelton",
-    role: "Software Engineer at Canonical's Ubuntu WSL Team",
+    bio: "Software Engineer at Canonical's Ubuntu WSL Team",
     location: "Campinas, Brazil",
     email: "mailto:carlosnsoliveira@gmail.com",
     github: "https://github.com/CarlosNihelton",
+    links: [
+      {
+        label: "LinkedIn",
+        href: "https://linkedin.com/in/carlos-nihelton",
+        kind: "linkedin",
+      },
+      {
+        label: "Launchpad",
+        href: "https://launchpad.net/~cnihelton",
+        kind: "launchpad",
+      },
+    ],
     image: "/authors/cnihelton.jpg",
   },
 ];
